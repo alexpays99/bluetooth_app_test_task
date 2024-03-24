34e63b131cc3a2bb540d7501414e6866ef2b4d80
alex.mosisa <alex.mosisa@chisw.com>
2024-03-24 22:55:41 +0200

    feat: completed bluetooth ap

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,60 +11,88 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demof',
+      title: 'Flutter Bluetooth Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const BluetoothApp(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class BluetoothApp extends StatefulWidget {
+  const BluetoothApp({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<BluetoothApp> createState() => _BluetoothAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _BluetoothAppState extends State<BluetoothApp> {
+  final List<BluetoothDevice> devicesList = [];
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    FlutterBluePlus.adapterState.listen((state) {
+      if (state == BluetoothAdapterState.off) {
+      } else if (state == BluetoothAdapterState.on) {
+        startScan();
+      }
     });
+  }
+
+  void startScan() {
+    FlutterBluePlus.startScan(timeout: const Duration(seconds: 4));
+
+    // Listen to scan results
+    FlutterBluePlus.scanResults.listen((results) {
+      for (ScanResult result in results) {
+        addDeviceToList(result.device);
+      }
+    });
+
+    FlutterBluePlus.stopScan();
+  }
+
+  void addDeviceToList(final BluetoothDevice device) {
+    if (!devicesList.contains(device)) {
+      setState(() {
+        devicesList.add(device);
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Flutter Bluetooth Demo'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: RefreshIndicator(
+        onRefresh: () =>
+            FlutterBluePlus.startScan(timeout: const Duration(seconds: 4)),
+        child: ListView.builder(
+          itemCount: devicesList.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(devicesList[index].platformName),
+              subtitle: Text(devicesList[index].remoteId.toString()),
+              trailing: ElevatedButton(
+                child: const Text('Connect'),
+                onPressed: () => connectToDevice(devicesList[index]),
+              ),
+            );
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: () => FlutterBluePlus.turnOn(),
+        child: const Icon(Icons.bluetooth),
       ),
     );
+  }
+
+  void connectToDevice(BluetoothDevice device) async {
+    await device.connect();
   }
 }
